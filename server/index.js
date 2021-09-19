fd75cf3eef04a4b64555c0b3015c8588d4372a7f
Frank Mckenna <fmckenna56330@gmail.com>
2021-09-19 20:25:20 +0100

    feat: Post task to databse

--- server/index.js
+++ server/index.js
@@ -4,21 +4,30 @@ const User = require('./models/User');
 require('dotenv').config();
 const Task = require('./models/Task');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors())
+
 const PORT = process.env.PORT || 3002
 
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true
 });
 
-app.get('/api', async (req, res) => {
-  const task = new Task({task: "Walk the dog"})
+app.post('/v2/api/tasks', async (req, res) => {
+  const taskText = req.body.tasks;
+  const task = new Task({task: taskText})
   try {
     await task.save();
   } catch(err) {
     console.log(err)
   }
+})
+
+app.get('/v2/api/tasks', async (req, res) => {
+  const data = await Task.findAll(res.params.task)
+  send.json(data)
 })
 
 
--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import Axios from 'axios';
 
 export const AddTask: React.FC = () => {
   const [task, setTask] = React.useState<string>('');
-  const [addTask, setAddTask] = React.useState<typeof task[] | []>([])
 
   const handleChange: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    setAddTask([...addTask, task]);
+    Axios.post('http://localhost:3002/v2/api/tasks', { tasks: task})
     setTask('')
   }
 
-
   return (
-        <form data-testid="addTask-form" onSubmit={handleChange}>
-          <input type='text' name ='task' placeholder='Enter a task...'
-           value={task} onChange={(e) => setTask(e.target.value)}>
-          </input>
-          <button type ='submit' value ='Add'>Add</button>
-        </form>
+    <div>
+      <form onSubmit={handleChange}>
+        <input required type='text' name ='task' placeholder='Enter a task...'
+          value={task} onChange={(e) => setTask(e.target.value)}>
+        </input>
+        <button type ='submit' value ='Add'>Add</button>
+      </form>
+    </div>
   )
 }
 
