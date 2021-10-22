5653acce1ea1349a56fd061e1c843c0473895000
Frank Mckenna <fmckenna56330@gmail.com>
2021-10-22 20:42:00 +0100

    chore: updated scripts

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
-const tasks = require('./routes/tasksRouter.tsx');
+const tasks = require('./routes/tasksRouter.js');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3002
