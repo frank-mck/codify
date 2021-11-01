f43c90a9a47e77e168214d8d2c96cbbb949702fe
Frank Mckenna <fmckenna56330@gmail.com>
2021-11-01 15:51:44 +0000

    chore: add bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -29,6 +30,10 @@ userSchema.pre("save", async function() {
   if(!this.isModified('password')) {
     next();
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 })
 
 module.exports = mongoose.model('User', userSchema);
