966953eb607217c1d7981cc5704def7d947374b7
Frank Mckenna <fmckenna56330@gmail.com>
2021-11-07 21:20:25 +0000

    test: delete task

--- server/__tests__/task.test.js
+++ server/__tests__/task.test.js
@@ -25,16 +25,24 @@ describe('Task', () => {
   test('Add task POST', async () => {
     const res = await Tasks.create({ task: "take out the trash" });
     await res.save();
-    
     expect(res.task).toBe("take out the trash");
   });
 
-  test('Update task POST', async () => {
+  test('Update task PUT', async () => {
     const res = await Tasks.create({ task: "take out the trash" });
     await res.save();
     const data = await Tasks.findOneAndUpdate({ _id: res._id }, { task: "Get bin bags" });
     await data.save();
-    const updatedTask = await Tasks.findById({_id: res._id })
+    const updatedTask = await Tasks.findById({_id: res._id });
     expect(updatedTask.task).toBe('Get bin bags');
+  });
+
+  test('Delete task DELETE', async () => {
+    const res = await Tasks.create({ task: "take out the trash" });
+    await res.save();
+    const task = await Tasks.findByIdAndDelete({ _id: res._id });
+    await task.delete();
+    const deletedTask = await Tasks.findById({ _id: res._id })
+    expect(deletedTask).toBe(null);
   })
 })
