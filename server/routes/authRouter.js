8c1472957ebe16f696d2cbab0f76087e453f1006
Frank Mckenna <fmckenna56330@gmail.com>
2022-06-18 23:55:07 +0100

    chore: add authentication middleware to authRouter

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const AuthCtrl = require('../controllers/AuthController');
+const { protect } = require('../middleware/auth');
 
-router.route('/users').get(AuthCtrl.getAllUsers)
+router.route('/users').get(protect, AuthCtrl.getAllUsers)
 
-router.route('/signup').post(AuthCtrl.signup);
+router.route('/signup').post(protect, AuthCtrl.signup);
 
-router.route('/signin').post(AuthCtrl.signin);
+router.route('/signin').post(protect, AuthCtrl.signin);
 
-router.route('/forgotpassword').post(AuthCtrl.forgotpassword);
+router.route('/forgotpassword').post(protect, AuthCtrl.forgotpassword);
 
-router.route('/resetpassword/:resetToken').put(AuthCtrl.resetpassword);
+router.route('/resetpassword/:resetToken').put(protect, AuthCtrl.resetpassword);
 
 module.exports = router;
